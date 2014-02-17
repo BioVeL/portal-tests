83d0287f2f0b4e96a3ba89c0f9ac634dcb362967
Jonathan Giddy <jongiddy@gmail.com>
2014-02-17 11:36:18 +0000

    Better end-story for ENM workflow

--- testBioVeL_ENM_default.py
+++ testBioVeL_ENM_default.py
@@ -80,25 +80,32 @@ class RunENMWorkflow(BaseTest):
             action_chains.move_to_element(continueButton).click().perform()
 
         # Code for BioSTIF interaction does not work consistently.
-        # Abandon run before we get there. Falling off the end of the test
-        # will cancel the running job.
+        # Abandon run before we get there.
 
-        # # BioSTIF - can't do much here, let's cancel
-        # with self.portal.waitForInteraction(300, 1):
+        self.removeRunAtURL(runURL)
+
+
+        # BioSTIF - the remaining code cancels the BioSTIF interaction,
+        # causing the workflow to finish. 
+        # with self.portal.waitForInteraction(300, 1) as iframe:
         #     # a dialog pops up with [msg_error_application_start_failed] - if it
         #     # appears dismiss it else after 20 seconds, hit the abort button
         #     try:
         #         self.portal.acceptAlert(30)
         #     except TimeoutException:
         #         pass
-        #     # continueButton = self.portal.wait(30).until(
-        #     #     expected_conditions.element_to_be_clickable(
-        #     #         (By.ID, 'user_cancel')
-        #     #         )
-        #     #     )
-        #     time.sleep(5)
-        #     continueButton = self.portal.find_element_by_id('user_cancel')
-        #     continueButton.click()
+        #     try:
+        #         self.portal.acceptAlert(30)
+        #     except TimeoutException:
+        #         pass
+        #     self.portal.switch_to_frame(iframe)
+        #     continueButton = self.portal.wait(60).until(
+        #         expected_conditions.element_to_be_clickable(
+        #             (By.ID, 'user_cancel')
+        #             )
+        #         )
+        #     action_chains = ActionChains(self.portal.browser)
+        #     action_chains.move_to_element(continueButton).click(continueButton).perform()
 
         # self.portal.waitForRunStatusContains("Finished", 300, 1)
 
@@ -107,7 +114,6 @@ class RunENMWorkflow(BaseTest):
         # self.portal.acceptAlert()
         # self.assertIn('Run was deleted', self.portal.getFlashNotice())
 
-        self.removeRunAtURL(runURL)
 
 # Firefox on Windows hangs on click of Run Workflow button using Selenium, but
 # not when running workflow manually
